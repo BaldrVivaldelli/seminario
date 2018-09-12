6254bd3d5c434c319112c13ef91010d204c49d84
Augusto Vivaldelli <aatv1@hotmail.com>
2018-09-11 21:31:30 -0300

    Se saca lo innecesario

--- BackEnd/customer-services/src/main/java/com/example/customerservices/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.customerservices.repository;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import com.example.customerservices.entities.Customer;
-
-public interface CustomerRepository extends ReactiveMongoRepository<Customer,String>{
-
-}
